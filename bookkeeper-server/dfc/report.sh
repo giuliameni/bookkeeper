7456d6a28ff2bd31dc5c0f6f477e886d49cf65da
giuliameni <130680703+giuliameni@users.noreply.github.com>
2023-06-13 17:55:02 +0200

    Update report.sh

--- bookkeeper-server/dfc/report.sh
+++ bookkeeper-server/dfc/report.sh
@@ -2,14 +2,9 @@
 
 #!/bin/sh
 
-BADUACLI="./dfc/ba-dua-cli-0.6.0-all.jar"
-BADUASER="./coverage.ser"
+CLI="./dfc/ba-dua-cli-0.6.0-all.jar"
+SER="./coverage.ser"
 CLASSES="./target/classes"
-BADUAXML="./target/badua.xml"
+XML="./target/badua.xml"
 
-java -jar ${BADUACLI} report    \
-        -input ${BADUASER}      \
-        -classes ${CLASSES}     \
-        -show-classes           \
-        -show-methods           \
-        -xml ${BADUAXML}
+java -jar ${CLI} report -input ${SER} -classes ${CLASSES} -show-classes -show-methods -xml ${XML}
