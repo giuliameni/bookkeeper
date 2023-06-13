c8fd5c8ebf7e2489af017f692e7859d1cc2d2b05
giuliameni <130680703+giuliameni@users.noreply.github.com>
2023-06-13 11:17:36 +0200

    Update and rename generate-report.sh to report.sh

---
+++ bookkeeper-server/dfc/report.sh
@@ -0,0 +1,15 @@
+
+
+#!/bin/sh
+
+BADUACLI="./dfc/ba-dua-cli-0.6.0-all.jar"
+BADUASER="./coverage.ser"
+CLASSES="./target/classes"
+BADUAXML="./target/badua.xml"
+
+java -jar ${BADUACLI} report    \
+        -input ${BADUASER}      \
+        -classes ${CLASSES}     \
+        -show-classes           \
+        -show-methods           \
+        -xml ${BADUAXML}
